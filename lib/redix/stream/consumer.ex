c16bc20ee97452b4ea0a27a2002019d54c835a2b
Jeffery Utter <jeff@jeffutter.com>
2018-07-16 16:38:58 -0500

    Add consumer-group support

--- lib/redix/stream/consumer.ex
+++ lib/redix/stream/consumer.ex
@@ -2,9 +2,15 @@ defmodule Redix.Stream.Consumer do
   @moduledoc """
   """
 
+  @type group_name :: String.t()
+  @type consumer_name :: String.t()
+
   @type state :: %{
           redix: Redix.Stream.redix(),
+          consumer_group_command_connection: Redix.Stream.redix(),
           stream: Redix.Stream.t(),
+          group_name: group_name(),
+          consumer_name: consumer_name(),
           handler: function() | mfa()
         }
 
@@ -14,7 +20,7 @@ defmodule Redix.Stream.Consumer do
   Starts a new GenServer of `Redix.Stream.Consumer`.
   """
   @spec start_link(Redix.Stream.redix(), Redix.Stream.t(), function() | mfa(), keyword()) ::
-          Supervisor.Spec.spec()
+          GenServer.on_start()
   def start_link(redix, stream, handler, opts \\ []) do
     GenServer.start_link(__MODULE__, {redix, stream, handler, opts})
   end
@@ -27,23 +33,48 @@ defmodule Redix.Stream.Consumer do
           {:ok, state}
   def init({redix, stream, handler, opts}) do
     timeout = Keyword.get(opts, :timeout, @default_timeout)
-    default_start_pos = Keyword.get(opts, :start_pos, "$")
-    tracker = Keyword.get(opts, :tracker, nil)
+    group_name = Keyword.get(opts, :group_name)
+    consumer_name = Keyword.get(opts, :consumer_name)
+    consumer_group_command_connection = Keyword.get(opts, :consumer_group_command_connection)
 
-    # If tracker specified, load our starting position
-    start_pos =
-      if tracker do
-        case Redix.command!(redix, ["GET", "stream_tracker:#{tracker}"]) do
-          nil -> default_start_pos
-          pos -> pos
-        end
-      else
-        default_start_pos
+    default_start_pos =
+      case group_name do
+        nil -> "$"
+        _ -> ">"
       end
+
+    start_pos = Keyword.get(opts, :start_pos, default_start_pos)
+
+    if consumer_name do
+      try do
+        start_pos =
+          case start_pos do
+            "$" -> "$"
+            ">" -> "$"
+            other -> other
+          end
+
+        Redix.command(redix, ["XGROUP", "CREATE", stream, group_name, start_pos])
+      rescue
+        e in Redix.Error ->
+          case e.message do
+            "BUSYGROUP Consumer Group name already exists" -> nil
+            _ -> raise e
+          end
+      end
+    end
 
     stream_more_data(timeout, start_pos)
 
-    {:ok, %{redix: redix, stream: stream, tracker: tracker, handler: handler}}
+    {:ok,
+     %{
+       redix: redix,
+       consumer_group_command_connection: consumer_group_command_connection,
+       stream: stream,
+       group_name: group_name,
+       consumer_name: consumer_name,
+       handler: handler
+     }}
   end
 
   @doc """
@@ -51,7 +82,74 @@ defmodule Redix.Stream.Consumer do
   """
   def handle_info(
         {:stream_more_data, timeout, start_pos},
-        %{redix: redix, stream: stream, handler: handler, tracker: tracker} = state
+        %{
+          redix: redix,
+          consumer_group_command_connection: consumer_group_command_connection,
+          stream: stream,
+          group_name: group_name,
+          consumer_name: consumer_name,
+          handler: handler
+        } = state
+      )
+      when not is_nil(group_name) and not is_nil(consumer_name) and
+             not is_nil(consumer_group_command_connection) do
+    # Wait for a number of messages to come in
+    {:ok, stream_results} =
+      Redix.command(
+        redix,
+        [
+          "XREADGROUP",
+          "GROUP",
+          group_name,
+          consumer_name,
+          "BLOCK",
+          timeout,
+          "STREAMS",
+          stream,
+          start_pos
+        ],
+        timeout: :infinity
+      )
+
+    # Process the results and get the next positions to consume from
+    for [^stream, items] <- stream_results do
+      {stream_items, next_pos} = stream_items_to_tuples(items, start_pos)
+
+      # Process the items
+      for stream_item = {id, _} <- stream_items |> Enum.reverse() do
+        case call_handler(handler, stream, stream_item) do
+          :ok ->
+            {:ok, _} =
+              Redix.command(consumer_group_command_connection, ["XACK", stream, group_name, id])
+
+          _ ->
+            nil
+        end
+      end
+
+      next_pos =
+        case start_pos do
+          ">" -> ">"
+          _ -> next_pos
+        end
+
+      # And stream more data...
+      stream_more_data(timeout, next_pos)
+    end
+
+    {:noreply, state}
+  end
+
+  @doc """
+  Handles a new message from a stream, dispatching it to the given handler.
+  """
+  def handle_info(
+        {:stream_more_data, timeout, start_pos},
+        %{
+          redix: redix,
+          stream: stream,
+          handler: handler
+        } = state
       ) do
     # Wait for a number of messages to come in
     {:ok, stream_results} =
@@ -62,22 +160,12 @@ defmodule Redix.Stream.Consumer do
       )
 
     # Process the results and get the next positions to consume from
-    for stream_result <- stream_results do
-      [^stream, items] = stream_result
-
-      {stream_items, next_pos} =
-        Enum.reduce(items, {[], start_pos}, fn [id, kvs], {msgs, _next_pos} ->
-          {[{id, kvs} | msgs], id}
-        end)
+    for [^stream, items] <- stream_results do
+      {stream_items, next_pos} = stream_items_to_tuples(items, start_pos)
 
       # Process the items
       for stream_item <- stream_items |> Enum.reverse() do
         call_handler(handler, stream, stream_item)
-      end
-
-      # If tracker specified, track our current position
-      if tracker do
-        Redix.command!(redix, ["SET", "stream_tracker:#{tracker}", next_pos])
       end
 
       # And stream more data...
@@ -95,6 +183,14 @@ defmodule Redix.Stream.Consumer do
   @spec call_handler(function(), Redix.Stream.t(), any()) :: any()
   defp call_handler(fun, stream, msg) do
     fun.(stream, msg)
+  end
+
+  @spec stream_items_to_tuples(list(list(String.t() | list(String.t()))), String.t()) ::
+          {list({String.t(), list(String.t())}), String.t()}
+  defp stream_items_to_tuples(items, start_pos) do
+    Enum.reduce(items, {[], start_pos}, fn [id, kvs], {msgs, _next_pos} ->
+      {[{id, kvs} | msgs], id}
+    end)
   end
 
   @spec stream_more_data(integer(), String.t()) :: :ok
--- test/redix/stream/consumer_test.exs
+++ test/redix/stream/consumer_test.exs
@@ -1,66 +1,92 @@
 defmodule Redix.Stream.ConsumerTest do
   use ExUnit.Case
+
   doctest Redix.Stream.Consumer
 
-  @test_stream "test_stream"
+  setup_all do
+    {:ok, cmd_connection} = Redix.start_link()
+
+    %{
+      cmd_connection: cmd_connection
+    }
+  end
+
+  setup %{cmd_connection: cmd_connection} do
+    stream_name = random_string(25)
+
+    on_exit(fn ->
+      {:ok, 1} = Redix.command(cmd_connection, ["DEL", stream_name])
+    end)
+
+    %{
+      stream_name: stream_name,
+      cmd_connection: cmd_connection
+    }
+  end
 
   @tag :integration
-  test "it should connect and stream a single message" do
+  test "it should connect and stream a single message", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} -> send(pid, {:streamed, stream, id, values}) end
       )
 
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
   end
 
   @tag :integration
-  test "it should connect and stream multiple messages" do
+  test "it should connect and stream multiple messages", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} -> send(pid, {:streamed, stream, id, values}) end
       )
 
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
     :timer.sleep(500)
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_2", "value_2"]}
-    assert_receive {:streamed, @test_stream, _id, ["key_3", "value_3"]}
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_2", "value_2"]}
+    assert_receive {:streamed, ^stream_name, _id, ["key_3", "value_3"]}
   end
 
   @tag :integration
-  test "it stream many messages at once" do
+  test "it stream many messages at once", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} ->
           # this runs in the consumer and blocks
           :timer.sleep(100)
@@ -73,106 +99,78 @@ defmodule Redix.Stream.ConsumerTest do
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_2", "value_2"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_3", "value_3"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_2", "value_2"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_3", "value_3"]}, 5_000
   end
 
   @tag :integration
-  test "it should start over without tracker specified" do
+  test "it streams messages to consumer groups but not the same message to multiple groups", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
+    # A stream must exist before a consumer group can be created
+    # https://github.com/antirez/redis/issues/4824
+    {:ok, _} = Redix.Stream.produce(cmd_connection, stream_name, "initial", "initial_value")
     {:ok, redix_1} = Redix.start_link()
     {:ok, redix_2} = Redix.start_link()
     {:ok, redix_3} = Redix.start_link()
     pid = self()
 
-    {:ok, consumer_pid} =
+    {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} ->
-          send(pid, {:streamed, stream, id, values})
-        end
+          # this runs in the consumer and blocks
+          :timer.sleep(100)
+          # further processing.
+
+          send(pid, {:streamed_consumer1, stream, id, values})
+
+          :ok
+        end,
+        group_name: "test",
+        consumer_name: "consumer1",
+        consumer_group_command_connection: redix_3
       )
 
-    # allow consumer time to connect
-    :timer.sleep(500)
-
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-
-    GenServer.stop(consumer_pid)
-
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
-
-    {:ok, _consumer_2_pid} =
+    {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
-        redix_3,
-        @test_stream,
+        redix_2,
+        stream_name,
         fn stream, {id, values} ->
-          send(pid, {:streamed_2, stream, id, values})
-        end
+          # this runs in the consumer and blocks
+          :timer.sleep(100)
+          # further processing.
+
+          send(pid, {:streamed_consumer2, stream, id, values})
+
+          :ok
+        end,
+        group_name: "test",
+        consumer_name: "consumer2",
+        consumer_group_command_connection: redix_3
       )
 
-    # allow consumer time to connect
-    :timer.sleep(500)
+    {:ok, msg_id1} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, msg_id2} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
+    {:ok, msg_id3} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_4", "value_4")
+    assert_receive {consumer1, ^stream_name, ^msg_id1, ["key_1", "value_1"]}, 5_000
+    assert_receive {consumer2, ^stream_name, ^msg_id2, ["key_2", "value_2"]}, 5_000
+    assert_receive {consumer3, ^stream_name, ^msg_id3, ["key_3", "value_3"]}, 5_000
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 1_000
-    refute_receive {:streamed_2, @test_stream, _id, ["key_2", "value_2"]}, 1_000
-    refute_receive {:streamed_2, @test_stream, _id, ["key_3", "value_3"]}, 1_000
-    assert_receive {:streamed_2, @test_stream, _id, ["key_4", "value_4"]}, 1_000
+    # Make sure each consumer handles at least one message
+    assert Enum.member?([consumer1, consumer2, consumer3], :streamed_consumer1)
+    assert Enum.member?([consumer1, consumer2, consumer3], :streamed_consumer2)
   end
 
-  @tag :integration
-  test "it should pick up where it left off when tracked" do
-    {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
-    {:ok, redix_3} = Redix.start_link()
-    pid = self()
-
-    {:ok, consumer_pid} =
-      Redix.Stream.Consumer.start_link(
-        redix_1,
-        @test_stream,
-        fn stream, {id, values} ->
-          send(pid, {:streamed, stream, id, values})
-        end,
-        tracker: "my_stream_tracker"
-      )
-
-    # allow consumer time to connect
-    :timer.sleep(500)
-
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-
-    GenServer.stop(consumer_pid)
-
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
-
-    {:ok, _consumer_2_pid} =
-      Redix.Stream.Consumer.start_link(
-        redix_3,
-        @test_stream,
-        fn stream, {id, values} ->
-          send(pid, {:streamed_2, stream, id, values})
-        end,
-        tracker: "my_stream_tracker"
-      )
-
-    # allow consumer time to connect
-    :timer.sleep(500)
-
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_4", "value_4")
-
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 1_000
-    assert_receive {:streamed_2, @test_stream, _id, ["key_2", "value_2"]}, 1_000
-    assert_receive {:streamed_2, @test_stream, _id, ["key_3", "value_3"]}, 1_000
-    assert_receive {:streamed_2, @test_stream, _id, ["key_4", "value_4"]}, 1_000
+  def random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
   end
 end
