bc01f4e97d970c288f93fca16a6e4ee5f1afec58
Jeffery Utter <jeff@jeffutter.com>
2018-07-16 22:13:41 -0500

    Create and cleanup stream keys between tests. Use random keys so tests don't write to keys used in other tests

--- test/redix/stream/consumer_test.exs
+++ test/redix/stream/consumer_test.exs
@@ -1,66 +1,92 @@
 defmodule Redix.Stream.ConsumerTest do
   use ExUnit.Case
+
   doctest Redix.Stream.Consumer
 
-  @test_stream "test_stream"
+  setup_all do
+    {:ok, cmd_connection} = Redix.start_link()
+
+    %{
+      cmd_connection: cmd_connection
+    }
+  end
+
+  setup %{cmd_connection: cmd_connection} do
+    stream_name = random_string(25)
+
+    on_exit(fn ->
+      {:ok, 1} = Redix.command(cmd_connection, ["DEL", stream_name])
+    end)
+
+    %{
+      stream_name: stream_name,
+      cmd_connection: cmd_connection
+    }
+  end
 
   @tag :integration
-  test "it should connect and stream a single message" do
+  test "it should connect and stream a single message", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} -> send(pid, {:streamed, stream, id, values}) end
       )
 
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
   end
 
   @tag :integration
-  test "it should connect and stream multiple messages" do
+  test "it should connect and stream multiple messages", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} -> send(pid, {:streamed, stream, id, values}) end
       )
 
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
     :timer.sleep(500)
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_2", "value_2"]}
-    assert_receive {:streamed, @test_stream, _id, ["key_3", "value_3"]}
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_2", "value_2"]}
+    assert_receive {:streamed, ^stream_name, _id, ["key_3", "value_3"]}
   end
 
   @tag :integration
-  test "it stream many messages at once" do
+  test "it stream many messages at once", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
     {:ok, redix_1} = Redix.start_link()
-    {:ok, redix_2} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} ->
           # this runs in the consumer and blocks
           :timer.sleep(100)
@@ -73,27 +99,32 @@ defmodule Redix.Stream.ConsumerTest do
     # allow consumer time to connect
     :timer.sleep(500)
 
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_1", "value_1")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_2", "value_2")
-    {:ok, _msg_id} = Redix.Stream.produce(redix_2, @test_stream, "key_3", "value_3")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
+    {:ok, _msg_id} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    assert_receive {:streamed, @test_stream, _id, ["key_1", "value_1"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_2", "value_2"]}, 5_000
-    assert_receive {:streamed, @test_stream, _id, ["key_3", "value_3"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_1", "value_1"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_2", "value_2"]}, 5_000
+    assert_receive {:streamed, ^stream_name, _id, ["key_3", "value_3"]}, 5_000
   end
 
   @tag :integration
-  test "it streams messages to consumer groups but not the same message to multiple groups" do
+  test "it streams messages to consumer groups but not the same message to multiple groups", %{
+    cmd_connection: cmd_connection,
+    stream_name: stream_name
+  } do
+    # A stream must exist before a consumer group can be created
+    # https://github.com/antirez/redis/issues/4824
+    {:ok, _} = Redix.Stream.produce(cmd_connection, stream_name, "initial", "initial_value")
     {:ok, redix_1} = Redix.start_link()
     {:ok, redix_2} = Redix.start_link()
     {:ok, redix_3} = Redix.start_link()
-    {:ok, redix_4} = Redix.start_link()
     pid = self()
 
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_1,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} ->
           # this runs in the consumer and blocks
           :timer.sleep(100)
@@ -111,7 +142,7 @@ defmodule Redix.Stream.ConsumerTest do
     {:ok, _pid} =
       Redix.Stream.Consumer.start_link(
         redix_2,
-        @test_stream,
+        stream_name,
         fn stream, {id, values} ->
           # this runs in the consumer and blocks
           :timer.sleep(100)
@@ -126,31 +157,20 @@ defmodule Redix.Stream.ConsumerTest do
         consumer_group_command_connection: redix_3
       )
 
-    # allow consumer time to connect
-    :timer.sleep(500)
+    {:ok, msg_id1} = Redix.Stream.produce(cmd_connection, stream_name, "key_1", "value_1")
+    {:ok, msg_id2} = Redix.Stream.produce(cmd_connection, stream_name, "key_2", "value_2")
+    {:ok, msg_id3} = Redix.Stream.produce(cmd_connection, stream_name, "key_3", "value_3")
 
-    {:ok, msg_id1} = Redix.Stream.produce(redix_4, @test_stream, "key_1", "value_1")
-    {:ok, msg_id2} = Redix.Stream.produce(redix_4, @test_stream, "key_2", "value_2")
-    {:ok, msg_id3} = Redix.Stream.produce(redix_4, @test_stream, "key_3", "value_3")
-    {:ok, msg_id4} = Redix.Stream.produce(redix_4, @test_stream, "key_4", "value_4")
-    {:ok, msg_id5} = Redix.Stream.produce(redix_4, @test_stream, "key_5", "value_5")
-    {:ok, msg_id6} = Redix.Stream.produce(redix_4, @test_stream, "key_6", "value_6")
+    assert_receive {consumer1, ^stream_name, ^msg_id1, ["key_1", "value_1"]}, 5_000
+    assert_receive {consumer2, ^stream_name, ^msg_id2, ["key_2", "value_2"]}, 5_000
+    assert_receive {consumer3, ^stream_name, ^msg_id3, ["key_3", "value_3"]}, 5_000
 
-    assert_receive {consumer1, @test_stream, ^msg_id1, ["key_1", "value_1"]}, 5_000
-    assert_receive {consumer2, @test_stream, ^msg_id2, ["key_2", "value_2"]}, 5_000
-    assert_receive {consumer3, @test_stream, ^msg_id3, ["key_3", "value_3"]}, 5_000
-    assert_receive {consumer4, @test_stream, ^msg_id4, ["key_4", "value_4"]}, 5_000
-    assert_receive {consumer5, @test_stream, ^msg_id5, ["key_5", "value_5"]}, 5_000
-    assert_receive {consumer6, @test_stream, ^msg_id6, ["key_6", "value_6"]}, 5_000
+    # Make sure each consumer handles at least one message
+    assert Enum.member?([consumer1, consumer2, consumer3], :streamed_consumer1)
+    assert Enum.member?([consumer1, consumer2, consumer3], :streamed_consumer2)
+  end
 
-    assert Enum.member?(
-             [consumer1, consumer2, consumer3, consumer4, consumer5, consumer6],
-             :streamed_consumer1
-           )
-
-    assert Enum.member?(
-             [consumer1, consumer2, consumer3, consumer4, consumer5, consumer6],
-             :streamed_consumer2
-           )
+  def random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
   end
 end
